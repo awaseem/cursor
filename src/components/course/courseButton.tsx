cfcf4a19c336767c7b4d05df03e050a8143d83c5
Ali Waseem <github@aliwaseem.com>
2020-03-10 18:59:58 -0600

    added button animation

--- src/components/course/courseButton.tsx
+++ src/components/course/courseButton.tsx
@@ -1,16 +1,79 @@
-import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import React, { useRef, useEffect, useState } from 'react'
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableWithoutFeedback,
+  Animated,
+  LayoutChangeEvent,
+} from 'react-native'
 import { iOSUIKit } from 'react-native-typography'
+import { colors } from '../../styles/color'
+
+var ACTION_TIMER = 400
+var COLORS = ['rgb(255,255,255)', colors.buttonSecondaryColor]
 
 export interface CourseButtonProps {
   text: string
 }
 
 export function CourseButton({ text }: CourseButtonProps) {
+  const _value = useRef(0)
+  const [pressAction, setPressAction] = useState(new Animated.Value(0))
+  const [buttonWidth, setButtonWidth] = useState(0)
+  const [buttonHeight, setButtonHeight] = useState(0)
+
+  useEffect(() => {
+    pressAction.addListener(({ value }) => (_value.current = value))
+  }, [])
+
+  function handlePressIn() {
+    Animated.timing(pressAction, {
+      duration: ACTION_TIMER,
+      toValue: 1,
+    }).start(animationActionComplete)
+  }
+
+  function handlePressOut() {
+    Animated.timing(pressAction, {
+      duration: _value.current * ACTION_TIMER,
+      toValue: 0,
+    }).start()
+  }
+
+  function buttonWidthHeightOnLayout(e: LayoutChangeEvent) {
+    setButtonWidth(e.nativeEvent.layout.width)
+    setButtonHeight(e.nativeEvent.layout.height)
+  }
+
+  function getProgressStyles() {
+    var width = pressAction.interpolate({
+      inputRange: [0, 1],
+      outputRange: [0, buttonWidth],
+    })
+    var bgColor = pressAction.interpolate({
+      inputRange: [0, 1],
+      outputRange: COLORS,
+    })
+    return {
+      width: width,
+      height: buttonHeight,
+      backgroundColor: bgColor,
+    }
+  }
+
+  function animationActionComplete() {}
+
   return (
-    <View style={styles.Container}>
-      <Text style={iOSUIKit.bodyEmphasizedWhiteObject}>{text}</Text>
-    </View>
+    <TouchableWithoutFeedback
+      onPressIn={handlePressIn}
+      onPressOut={handlePressOut}
+    >
+      <View style={styles.Container} onLayout={buttonWidthHeightOnLayout}>
+        <Animated.View style={[styles.bgFill, getProgressStyles()]} />
+        <Text style={iOSUIKit.bodyEmphasizedWhiteObject}>{text}</Text>
+      </View>
+    </TouchableWithoutFeedback>
   )
 }
 
@@ -20,7 +83,13 @@ const styles = StyleSheet.create({
     height: 60,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#1EA896',
+    backgroundColor: colors.buttonMainColor,
+    borderRadius: 25,
+  },
+  bgFill: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
     borderRadius: 25,
   },
 })
--- src/styles/color.ts
+++ src/styles/color.ts
@@ -2,4 +2,8 @@ export const colors = {
   indicatorColor: '#FF715B',
 
   separtorColor: '#4C5454',
+
+  buttonMainColor: '#4C5454',
+
+  buttonSecondaryColor: '#1EA896',
 }
