4faaaf32d90e61855319feb2cf5d65a7deb0f10c
Ali Waseem <github@aliwaseem.com>
2020-03-19 19:59:46 -0600

    added presistence layer for store redux store

--- App.tsx
+++ App.tsx
@@ -3,19 +3,22 @@ import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { AppearanceProvider } from 'react-native-appearance'
 import { NavigationContainer } from '@react-navigation/native'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import { RootStackScreen } from './src/navigation/navigation'
-import { store } from './src/redux/rootReducer'
+import { store, persistedStore } from './src/redux/rootReducer'
 
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <SafeAreaProvider>
-          <AppearanceProvider>
-            <RootStackScreen />
-          </AppearanceProvider>
-        </SafeAreaProvider>
-      </NavigationContainer>
+      <PersistGate loading={null} persistor={persistedStore}>
+        <NavigationContainer>
+          <SafeAreaProvider>
+            <AppearanceProvider>
+              <RootStackScreen />
+            </AppearanceProvider>
+          </SafeAreaProvider>
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   )
 }
--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,16 +1,51 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { AsyncStorage } from 'react-native'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import {
+  combineReducers,
+  configureStore,
+  getDefaultMiddleware,
+} from '@reduxjs/toolkit'
 import { courseList, selectedCourse } from './courseSlices'
+import { profile } from './profileSlice'
+
+const ASYNC_STORAGE_KEY = 'root_storage'
+const BLACKLIST = ['courses']
 
 export const rootReducer = combineReducers({
   courses: combineReducers({
     courseList: courseList.reducer,
     selectedCourse: selectedCourse.reducer,
   }),
+  profile: profile.reducer,
 })
 
+const persistedReducer = persistReducer(
+  {
+    key: ASYNC_STORAGE_KEY,
+    storage: AsyncStorage,
+    blacklist: BLACKLIST,
+  },
+  rootReducer,
+)
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 })
+export const persistedStore = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch
 export type AppState = ReturnType<typeof rootReducer>
