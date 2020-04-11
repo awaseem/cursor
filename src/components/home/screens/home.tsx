dfad47b674878f90994d9e4156f93e8af9a57dd6
Ali Waseem <github@aliwaseem.com>
2020-04-11 16:06:47 -0600

    updated scroll to top

--- src/components/home/screens/home.tsx
+++ src/components/home/screens/home.tsx
@@ -101,9 +101,9 @@ export function Home({
     if (path) {
       setScrollHomeListToTop(true)
       setTimeout(() => {
-        setScrollHomeListToTop(false)
         getCourses(path)
-      }, 500)
+        setScrollHomeListToTop(false)
+      }, 100)
     }
   }, [selectedSubject])
 
